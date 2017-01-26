50748be13341db35b806a497a0bb09eb48acc585
n1cfury <fukugetin@gmail.com>
2017-01-25 19:58:56 -0800

    organized giagantor output in order to prep for functions.  I'm going to put off the functions for the night.

--- domain-reputation.py
+++ domain-reputation.py
@@ -7,9 +7,9 @@ How the output should look: (repeat for each service e.g. whois, virustotal, etc
 		<parsed results>
 		
 Next iteration(s)
-	Start working on output
+	Parsing the output and make it neater
 
-NO FUNCTIONS YET.  GETTING IT TO WORK WITH A PRE-DEFINED DOMAIN
+
 '''
 import pythonwhois, senderbase 					#Third party modules that require pip install
 from senderbase import SenderBase
@@ -21,13 +21,16 @@ APIKEY_VIRUSTOTAL = '### VIRUSTOTAL API KEY ###'
 
 #WHOIS QUERY								
 domain_query = pythonwhois.get_whois(domain)
+print "[+] Report for "+domain+" completed......"
+print ""
 
-'''
 #VIRUSTOTAL QUERIES
 #Submits a site to Virustotal (USE THIS FOR NEW SITES)
 params = {'apikey': APIKEY_VIRUSTOTAL, 'url': domain}
 response = requests.post('https://www.virustotal.com/vtapi/v2/url/scan', data=params)
 vts_response = response.json()
+print "[+] Virustotal submittal for "+domain+" has completed..........."
+print vts_response
 
 
 #Retrieving results
@@ -39,29 +42,18 @@ params = {'apikey': APIKEY_VIRUSTOTAL, 'resource': domain}
 response = requests.post('https://www.virustotal.com/vtapi/v2/url/report',
   params=params, headers=headers)
 vtr_response = response.json()
-'''
+print "[+] Virustotal retrieved results for "+domain+" ..............."
+print vtr_response
+
 
 #SENDERBASE QUERIES
-
 sb = SenderBase(timeout=30)
-my_result = sb.lookup(domain)
-
-
-'''
-#COMPLETE SCAN RESULTS
-print "[+] Report for "+domain+" completed......"
-print ""
-print "[+] whois results for "+domain+ " ..........."
-print domain_query
-print ""
-print "[+] Virustotal submittal for "+domain+" has completed..........."
-print vts_response
-print ""
-print "[+] Virustotal retrieved results for "+domain+" ..............."
-print vtr_response
-print "[+] Virustotal retrieved results for "+domain+" ..............."
-print vtr_response
-print ""
+sb_result = sb.lookup(domain)
 print "[+] Senderbase retrived results for "+domain+" ............"
+print sb_result
 
-'''
+
+
+
+
+
