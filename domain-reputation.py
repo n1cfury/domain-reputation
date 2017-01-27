855ef830338134318254c12754cb71318d0bcf70
n1cfury <fukugetin@gmail.com>
2017-01-27 13:38:00 -0800

    Created functions for each query and debugging issues with arguments.  The Pythonwhois module works when you run 'python domain-reputation.py <domain>' but fails when you try to run the script as an executable.  I removed the excess comments at the top leaving a TODO statement for the next iteration.  After the functions get worked out, I am going to parse the output to make it neater and eventually find a way to do a whois query without the separate module.

--- domain-reputation.py
+++ domain-reputation.py
@@ -1,59 +1,60 @@
-#!/usr/bin/python
-'''
-What does this do?
-	#Checks a domain's reputation from multiple sources
-How the output should look: (repeat for each service e.g. whois, virustotal, etc )
-		print "[+]   Retrieved <service> information for <domain>"
-		<parsed results>
-		
-Next iteration(s)
-	Parsing the output and make it neater
+#TODO:  Parse the data to make the output look neater
 
-
-'''
-import pythonwhois, senderbase 					#Third party modules that require pip install
+import pythonwhois, senderbase, re, sys, requests			#Third party modules that require pip install
 from senderbase import SenderBase
-import re, sys, requests
 
+domain = sys.argv[1]
+APIKEY_VIRUSTOTAL = '060784e151263e1e6157d2565e31eaf069f2a7f8b84482f9cd5bdcc873c6c2e9'
 
-domain = 'n1cfury.com'
-APIKEY_VIRUSTOTAL = '### VIRUSTOTAL API KEY ###'
+usage = "python domain-reputation.py <domain name> "
 
-#WHOIS QUERY								
-domain_query = pythonwhois.get_whois(domain)
-print "[+] Report for "+domain+" completed......"
-print ""
+def banner():
+	print "################################################################"
+	print "###############	  Domain Reputation tool		###############"
+	print "###############			by n1cFury			    ###############"
+	print "################################################################"
+
+#WHOIS QUERY	
+def WhoisQuery(domain):							
+	domain_query = pythonwhois.get_whois(domain)
+	print ""
+	print "[+]               WHOIS Report for "+domain+" completed......"
+	print ""
+	print domain_query
+	print "##############################################################################"
 
 #VIRUSTOTAL QUERIES
-#Submits a site to Virustotal (USE THIS FOR NEW SITES)
-params = {'apikey': APIKEY_VIRUSTOTAL, 'url': domain}
-response = requests.post('https://www.virustotal.com/vtapi/v2/url/scan', data=params)
-vts_response = response.json()
-print "[+] Virustotal submittal for "+domain+" has completed..........."
-print vts_response
-
-
-#Retrieving results
-headers = {
-  "Accept-Encoding": "gzip, deflate",
-  "User-Agent" : "gzip,  My Python requests library example client or username"
-  }
-params = {'apikey': APIKEY_VIRUSTOTAL, 'resource': domain}
-response = requests.post('https://www.virustotal.com/vtapi/v2/url/report',
-  params=params, headers=headers)
-vtr_response = response.json()
-print "[+] Virustotal retrieved results for "+domain+" ..............."
-print vtr_response
-
+def VirTotalQuery(domain):
+	headers = {
+	  "Accept-Encoding": "gzip, deflate",
+	  "User-Agent" : "gzip,  My Python requests library example client or username"
+	  }
+	params = {'apikey': APIKEY_VIRUSTOTAL, 'resource': domain}
+	response = requests.post('https://www.virustotal.com/vtapi/v2/url/report',
+	  params=params, headers=headers)
+	vtr_response = response.json()
+	print "[+]               Virustotal retrieved results for "+domain+" ..............."
+	print ""
+	print vtr_response
+	print "##############################################################################"
 
 #SENDERBASE QUERIES
-sb = SenderBase(timeout=30)
-sb_result = sb.lookup(domain)
-print "[+] Senderbase retrived results for "+domain+" ............"
-print sb_result
+def SBaseQuery(domain):
+	sb = SenderBase(timeout=30)
+	sb_result = sb.lookup(domain)
+	print "[+]               Senderbase retrived results for "+domain+" ............"
+	print ""
+	print sb_result
+	print "##############################################################################"
 
+def main():
+	if len(sys.argv) == 1:
+		banner()
+		WhoisQuery(domain)
+		VirTotalQuery(domain)
+		SBaseQuery(domain)
+	else:
+		print usage
 
-
-
-
-
+if __name__ == "__main__":
+  main()
